446117a4bd5e18ea7f2091ee1f6fcab75354995b
Phil <pnomolos@gmail.com>
2014-02-04 22:09:06 -0800

    Proof-of-concept working.

--- public/js/api.js
+++ public/js/api.js
@@ -1,3 +1,10 @@
 function Bulb() {
 	var self = $.observable(this);
+	var bulbs = {};
+	
+	self.add = function(bulb) {
+		console.log("Adding bulb");
+		bulbs[bulb.id] = bulb;
+		self.trigger('add', bulb);
+	}
 }
--- public/js/app.js
+++ public/js/app.js
@@ -11,20 +11,31 @@
 		console.log('WebSocket Error ' + error);
 	};
 	
-	console.log('here');
-
-	// Log messages from the server
+	connection.onopen = function () {
+		console.log("Opening connection");
+		connection.send('{"message": "get_light_state", "payload": null}');
+	};
+	
 	connection.onmessage = function (e) {
-		console.log('Server: ' + e.data);
 		var resp = JSON.parse(e.data);
 		console.log(resp);
+		switch (resp.message) {
+			case 'light_state':
+				$.each(resp.params, function(){
+					window.bulb.add(this);
+				})
+				break;
+		}
 	};
 	
 	var template = $("[type='html/bulb']").html(),
 		root = $('#bulb-list');
-		
-	connection.onopen = function () {
-		console.log("Opening connection");
-		connection.send('Ping'); // Send the message 'Ping' to the server
-	};
+	
+	bulb.on("add", add);
+	
+	// Private
+	function add(bulb) {
+		console.log(bulb);
+		var el = $($.render(template, bulb)).appendTo(root);
+	}
 }()
